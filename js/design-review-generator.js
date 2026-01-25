ac67e213cc93fc1695dff9a9e4c7e0f450a128fc
JAK-Services <140812833+JAK-Services@users.noreply.github.com>
2026-01-25 17:53:29 +0100

    Update design-review-generator.js

--- js/design-review-generator.js
+++ js/design-review-generator.js
@@ -131,6 +131,7 @@
 		<meta charset="utf-8"/>
 		<meta name="viewport" content="width=device-width, initial-scale=1"/>
 		<title>PCB Design Review Checklist</title>
+		<link rel="stylesheet" href="https://jak-services.github.io/css/main.css"/>
 		<link rel="stylesheet" href="https://jak-services.github.io/css/design-review.css"/>
 	
 		<style>
